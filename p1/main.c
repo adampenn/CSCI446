6dd5eb46633bdfcac6d67a2a8e19a5c3e7ac5ecb
Adam J. Penn <madannep@gmail.com>
2016-02-29 15:46:41 -0800

    uploading main.c

---
+++ p1/main.c
@@ -0,0 +1,15 @@
+//
+// Adam J. Penn
+// Thomas Greenman
+//
+
+using namespace std;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char **argv){
+  printf("lol");
+}
